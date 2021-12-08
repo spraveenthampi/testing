5f2411e35c399b6ddc6fd2ed09ac28a921951b86
Ravikumar Kubusada <ravikumar.kubusada@mpoweredhealth.com>
2021-12-08 15:23:50 +0530

    log testing

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/test/")
+@Slf4j
 public class TestController {
 
 	@GetMapping("hello")
 	public String test() {
+		log.debug("hello world logging: DEBUG -- JSON");
+		log.error("hello world logging: ERROR -- JSON");
+		log.info("hello world logging: INFO -- JSON");
+		log.warn("hello world logging: WARN -- JSON");
 		return "hello world";
 	}
 
