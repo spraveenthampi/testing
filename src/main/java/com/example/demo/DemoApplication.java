f9094bd609ee8dbe1f3acbb635e8207cb13f1807
Praveen <spraveenthampi@gmail.com>
2022-02-17 10:02:31 +0530

    Adding sysout

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	DemoApplication(){
+		//simulating multiline error
+		String linetest = "Error parsing HTTP request header \n" +
+				  "at org.apache.coyote.http11.Http11InputBuffer.parseRequestLine(Http11InputBuffer.java:419) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" + 
+				  "at org.apache.coyote.AbstractProcessorLight.process(AbstractProcessorLight.java:65) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" +
+				  "at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:895) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" + 
+				  "at java.base/java.lang.Thread.run(Thread.java:834) ~[na:na]";
+		System.out.println(linetest);
+	}
 
 	public static void main(String[] args) {
 				
