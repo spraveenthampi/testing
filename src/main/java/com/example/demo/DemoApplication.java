21b19dc1c1ea3fdb7a476fb17e928c310dd93087
Praveen <spraveenthampi@gmail.com>
2022-02-17 10:11:21 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 	
 	DemoApplication(){
 		//simulating multiline error
-		String linetest = "Error parsing HTTP request header \n" +
+		String linetest = "2022-02-17 04:39:27.971  INFO 1 --- [           main] com.example.demo.DemoApplication         : Error parsing HTTP request header \n" +
 				  "at org.apache.coyote.http11.Http11InputBuffer.parseRequestLine(Http11InputBuffer.java:419) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" + 
 				  "at org.apache.coyote.AbstractProcessorLight.process(AbstractProcessorLight.java:65) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" +
 				  "at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:895) ~[tomcat-embed-core-9.0.55.jar!/:na] \n" + 
