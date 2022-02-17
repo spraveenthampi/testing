eb367da2a6d994a81d0575c0cf4b7e4a6ca16901
Praveen <spraveenthampi@gmail.com>
2022-02-17 09:37:10 +0530

    Reverting the changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public123 class123 DemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
